2ab5f59ceabee8dbe5bda6cd43d193b85299de50
Aman Patel <apofficial2002@gmail.com>
2022-08-18 21:05:29 +0530

    part2 completed

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,8 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DataBaseMethods {
-  getUsersByUserNmae(String username) {}
+  static getUsersByUserNmae(String username) async {
+    return await FirebaseFirestore.instance
+        .collection("users")
+        .where("name", isEqualTo: username)
+        .get();
+  }
+
   static uploadUserInfo(userMap) {
     FirebaseFirestore.instance.collection("users").add(userMap);
+  }
+
+  static createChatRoom(String chatroomId, chatRoomMap) {
+    FirebaseFirestore.instance.collection("chatroom").doc(chatroomId);
   }
 }
--- lib/views/search.dart
+++ lib/views/search.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: unrelated_type_equality_checks
+
 import 'package:chatapp/projectwidgets/appWidgets.dart';
+import 'package:chatapp/services/database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,11 +15,50 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   TextEditingController searchTextEditingController = TextEditingController();
+  late QuerySnapshot<Map<String, dynamic>> searchSnapShot;
+  initStateSearch() {
+    DataBaseMethods.getUsersByUserNmae(searchTextEditingController.text)
+        .then((val) {
+      setState(() {
+        searchSnapShot = val;
+      });
+    });
+  }
+
+  // createChatroomAndStartConversation(String username, ) {
+  //   List<String> users = [];
+  //   DataBaseMethods.createChatRoom(chatroomId, chatRoomMap)
+  // }
+
+  Widget searchList() {
+    return searchSnapShot != Null
+        ? SizedBox(
+            height: 29,
+            child: ListView.builder(
+                itemCount: searchSnapShot.docs.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return SearchTile(
+                    userName: searchSnapShot.docs[index].data()["name"],
+                    userEmail: searchSnapShot.docs[index].data()["email"],
+                  );
+                }),
+          )
+        : Container();
+  }
+
+  @override
+  void initState() {
+    // initStateSearch();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBarMain(context),
       body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
         child: Column(children: [
           Container(
             color: const Color(0x54FFFFFF),
@@ -30,22 +73,68 @@ class _SearchScreenState extends State<SearchScreen> {
                       hintStyle: TextStyle(color: Colors.white54),
                       border: InputBorder.none),
                 )),
-                Container(
-                    height: 40,
-                    width: 40,
-                    decoration: BoxDecoration(
-                      gradient: const LinearGradient(colors: [
-                        Color(0x36FFFFFF),
-                        Color(0x0FFFFFFF),
-                      ]),
-                      borderRadius: BorderRadius.circular(40),
-                    ),
-                    padding: const EdgeInsets.all(12),
-                    child: Image.asset("assets/images/search_white.png")),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                        gradient: const LinearGradient(colors: [
+                          Color(0x36FFFFFF),
+                          Color(0x0FFFFFFF),
+                        ]),
+                        borderRadius: BorderRadius.circular(40),
+                      ),
+                      padding: const EdgeInsets.all(12),
+                      child: Image.asset("assets/images/search_white.png")),
+                ),
               ],
             ),
           )
         ]),
+      ),
+    );
+  }
+}
+
+class SearchTile extends StatelessWidget {
+  final String userName;
+  final String userEmail;
+  SearchTile({required this.userName, required this.userEmail});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                userName,
+                style: mediumTextStyle(),
+              ),
+              Text(
+                userEmail,
+                style: mediumTextStyle(),
+              )
+            ],
+          ),
+          const Spacer(),
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.blue, borderRadius: BorderRadius.circular(30)),
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+            child: GestureDetector(
+              onTap: () {},
+              child: Text(
+                "Message",
+                style: mediumTextStyle(),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
